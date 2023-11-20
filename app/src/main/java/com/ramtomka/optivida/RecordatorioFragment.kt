c4b4445b874266607da798e053456df4866f16d4
Ramiro Pc <rvidallumbreras@gmail.com>
2023-11-20 17:01:08 -0600

    Creacion de un menú de navegación en la parte inferior de la app

---
+++ app/src/main/java/com/ramtomka/optivida/RecordatorioFragment.kt
@@ -0,0 +1,12 @@
+package com.ramtomka.optivida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RecordatorioFragment : Fragment(R.layout.fragment_recordatorio) {
+
+}
