59ceeba931f92611ed7534412791eea8019332d8
Jose Carlos Mejia Ramirez <bidios_comedia_53@hotmail.com>
2023-11-23 12:53:32 -0600

    Creacion de UI para recuperar contraseña y clase 'CambiarPass' para redirigir

--- app/src/main/java/com/ramtomka/optivida/CambiarPass.kt
+++ app/src/main/java/com/ramtomka/optivida/CambiarPass.kt
@@ -4,27 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
-class LoginActivity : AppCompatActivity() {
+class CambiarPass : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_cambiarpassword)
 
         val btnLogin = findViewById<Button>(R.id.btn)
-
-        val btnOlvidar = findViewById<TextView>(R.id.OlvidarPass)
 
         btnLogin.setOnClickListener{
             //INICIAR OTRA ACTIVIDAD
             val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        btnOlvidar.setOnClickListener{
-            //INICIAR OTRA ACTIVIDAD
-            val intent = Intent(this,CambiarPass::class.java)
             startActivity(intent)
             finish()
         }
