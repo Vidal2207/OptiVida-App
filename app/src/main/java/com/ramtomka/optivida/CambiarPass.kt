5620ba1a45355e5a4c0e0ecf8d89ad66c20cc0cf
Jose Carlos Mejia Ramirez <bidios_comedia_53@hotmail.com>
2023-11-25 11:30:59 -0600

    Se juntaron los cambios de master a devCarlos para poder trabajar sin afectar lo ya realizado

--- app/src/main/java/com/ramtomka/optivida/CambiarPass.kt
+++ app/src/main/java/com/ramtomka/optivida/CambiarPass.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class CambiarPass : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,7 @@ class CambiarPass : AppCompatActivity() {
         setContentView(R.layout.activity_cambiarpassword)
 
         val btnLogin = findViewById<Button>(R.id.btn)
+        val btnReenviar = findViewById<TextView>(R.id.textView5)
 
         btnLogin.setOnClickListener{
             //INICIAR OTRA ACTIVIDAD
@@ -18,5 +21,12 @@ class CambiarPass : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        btnReenviar.setOnClickListener{
+            val alert:String = "Se ha reenviado un nuevo codigo"
+            Toast.makeText(this,alert, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
